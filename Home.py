d8e8853b0f6a8177f9f0aa2b54ade2de7e64f3e5
vijaysai1510 <101976856+vijaysai1510@users.noreply.github.com>
2024-06-12 00:31:09 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="TranslaBuddy",
+    page_icon="translate.png"
+)
+st.title(":house: TranslaBuddy")
+
+
+st.write(""" ##### At TranslaBuddy, we believe that language should never be a barrier to communication, understanding, and connection. In a world where people from diverse linguistic backgrounds come together, the ability to express oneself and understand others is invaluable.
+
+##### Our mission is simple: to break down language barriers and facilitate seamless communication across the globe. We harness the power of Machine Learning and API calling technology to provide you with accurate and reliable translation services. Whether you're a business looking to expand internationally, a traveler exploring new cultures, or an individual seeking to connect with others, TranslateMate is here to help.""")
